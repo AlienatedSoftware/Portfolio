7cea9673fa360ee7d41d1b129aeb009e0719a001
AlienatedSoftware <aidancgart@gmail.com>
2021-05-28 19:43:37 +0000

    built-in torus looping animation

--- main.js
+++ main.js
@@ -8,14 +8,28 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg'),
-})
-
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
-
-renderer.render(scene, camera);
+  });
+  
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.position.setZ(30);
+  
+  renderer.render(scene, camera);
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0x006800, wireframe: true });
-const torus = new THREE.Mesh(geometry, material);
+const material = new THREE.MeshBasicMaterial({ color: 0x009900, wireframe: true });
+const torus = new THREE.Mesh( geometry, material );
+
+scene.add(torus)
+
+function animate() {
+    requestAnimationFrame(animate);
+  
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.005;
+    torus.rotation.z += 0.01;
+
+    renderer.render(scene, camera);
+  }
+
+animate();
